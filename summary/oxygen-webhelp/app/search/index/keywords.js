4714c628c1dcc862af7aa5f3a191a2df8d83093b
xuniLDude <jon.haener.sys@gmail.com>
2021-02-24 21:45:23 +0800

    Site
    Commit

---
+++ summary/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,27 @@
+define(function() {var keywords=[{w:"Colonisation",p:["p0","p1"]},{w:"process",p:["p0"]},{w:"Step",p:["p1","p2","p3"]},{w:"1:",p:["p1"]},{w:"2:",p:["p2"]},{w:"Political",p:["p2","p4"]},{w:"incorporation",p:["p2"]},{w:"3:",p:["p3"]},{w:"Nationalist",p:["p3"]},{w:"reaction",p:["p3"]},{w:"tool",p:["p4"]},{w:"Language",p:["p5","p7","p10","p16"]},{w:"policies",p:["p5"]},{w:"and",p:["p5"]},{w:"English",p:["p5","p18","p19"]},{w:"teaching",p:["p5"]},{w:"(ELT)",p:["p5"]},{w:"Linguistic",p:["p6","p17"]},{w:"consequences",p:["p6"]},{w:"Global",p:["p7"]},{w:"Case",p:["p8"]},{w:"studies",p:["p8"]},{w:"Introduction",p:["p9"]},{w:"Education",p:["p10"]},{w:"Learning",p:["p11"]},{w:"points",p:["p11"]},{w:"Codification",p:["p12"]},{w:"of",p:["p12","p14","p18"]},{w:"new",p:["p12"]},{w:"variety",p:["p12"]},{w:"Development",p:["p13","p18","p19"]},{w:"stages",p:["p13"]},{w:"Dialect",p:["p14"]},{w:"levelling",p:["p14"]},{w:"internal",p:["p14"]},{w:"variations",p:["p14"]},{w:"Factsheet",p:["p15"]},{w:"contact",p:["p16"]},{w:"situations",p:["p17"]},{w:"Paradoxical",p:["p18"]},{w:"varieties",p:["p19"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 0];
+ph["p2"]=[2, 4, 5, 6];
+ph["p3"]=[2, 7, 8, 9];
+ph["p4"]=[5, 10];
+ph["p5"]=[11, 12, 13, 14, 15, 16];
+ph["p6"]=[17, 18];
+ph["p7"]=[19, 11];
+ph["p8"]=[20, 21];
+ph["p9"]=[22];
+ph["p10"]=[11, 23];
+ph["p12"]=[26, 27, 28, 29];
+ph["p11"]=[24, 25];
+ph["p14"]=[32, 33, 27, 34, 35];
+ph["p13"]=[30, 31];
+ph["p16"]=[11, 37];
+ph["p15"]=[36];
+ph["p18"]=[39, 30, 27, 14];
+ph["p17"]=[17, 38];
+ph["p19"]=[14, 40, 30];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ summary/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {0:-1,15:19,12:19,13:19,19:-1,17:19,14:19,1:0,4:10,8:0,5:10,7:-1,10:-1,18:19,16:19,9:-1,11:-1,3:0,2:0};
+});
