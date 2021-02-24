21614b69a2eb5376b9371664a6d50b8faff2a438
xuniLDude <jon.haener.sys@gmail.com>
2021-02-24 20:49:18 +0800

    First commit;

---
+++ oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+define(function () {
+return ["toolk_word_building_4.html@@@Word building [4]@@@Words (lexemes) are built from morphemes - smaller parts with own meaning. Compounding and derivation are word building processes. Inflection grammatically adjusts words...","toolk_word_classes_3.html@@@Word classes [3]@@@Word classes classify words of sentence structures. All members of a word class behave in the same way. Use the substitution rule to test word class behaviour..."];
+});
