2a7b6a6f2c4e1e578928de22fda18606a9d07c9f
xuniLDude <jon.haener.sys@gmail.com>
2021-03-02 22:40:47 +0800

    Chapter 4 added

--- summary/oxygen-webhelp/app/config.js
+++ summary/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210224232845';
+                args = '20210302223755';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
