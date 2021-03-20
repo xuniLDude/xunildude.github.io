10f1bbcc0375fb82bd9a35592f1138f6ff1cbda7
xuniLDude <jon.haener.sys@gmail.com>
2021-03-20 18:46:41 +0800

    Chap 6 added;

---
+++ summary/oxygen-webhelp/app/nav-links/json/b1_t4_global_language-d633e383.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"4 Global Language","href":"b1t4_globallanguage\/b1-t4-statistics.html","menu":{"hasChildren":true},"tocID":"tocId-d633e394","next":"tocId-d633e394"}]});
---
+++ summary/oxygen-webhelp/app/nav-links/json/b1_t4_global_language-d661e384.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"4 Global Language","href":"b1t4_globallanguage\/b1-t4-statistics.html","menu":{"hasChildren":true},"tocID":"tocId-d661e395","next":"tocId-d661e395"}]});
