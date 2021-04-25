1680bb17e1a79abd19999788c2dac4e19295dc5b
xuniLDude <jon.haener.sys@gmail.com>
2021-04-25 12:42:39 +0800

    Commit

--- summary/oxygen-webhelp/app/config.js
+++ summary/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210320184421';
+                args = '20210328150148';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
